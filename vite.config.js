4a8387c8274bea898ec153c5ad088c451365d3d6
danielagblo <agblod27@gmail.com>
2026-01-27 20:44:51 +0000

    Add server and preview allowedHosts configuration

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'skytech-ltfxk.ondigitalocean.app' // Add your DigitalOcean URL here
+    ]
+  },
+  preview: {
+    allowedHosts: true,
+    port: 8080,
+    host: true
+  }
 })
