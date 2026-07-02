cde99695fb2ef1ef3a99b36ff3f08a7ee9107349
oldVinyl <neamewu@gmail.com>
2026-07-02 11:56:31 +0000

    increase navbar z-index

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-screen flex flex-col overflow-x-hidden">
-        <NavigationPC className="fixed top-10 left-0 z-10 w-screen py-1 bg-white/10 backdrop-blur-md" />
+        <NavigationPC className="fixed top-10 left-0 z-20 w-screen py-1 bg-white/10 backdrop-blur-md" />
         <main className="flex-1 h-full">
           {children}
         </main>
